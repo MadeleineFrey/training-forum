36f5a77ad51d0559063feb19105127c7bcf7d3f4
Madde <Madde.frey@gmail.com>
2022-02-05 18:01:54 +0000

    deployment error fix

--- traint/settings.py
+++ traint/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['traint-forum.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['traint-forum.herokuapp.com', 'localhost']
 
 
 # Application definition
