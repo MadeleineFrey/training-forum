6d4e70584bd087d8bb0bbcc4aacfbf202c6a782e
Madde <Madde.frey@gmail.com>
2022-02-08 14:40:12 +0000

    Add comment models in models.py and make migration

--- forum/models.py
+++ forum/models.py
@@ -1,28 +1,45 @@
 from django.db import models
 from django.contrib.auth.models import User
-#from cloudinary.models import CloudinaryField
+# from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
-STATUS = ((0, "Draft"), (1, "Published"))
+STATUS = ((0, 'Draft'), (1, 'Published'))
 
 
 class Question(models.Model):
-    ''' X '''
+    """ X """
     title = models.CharField(max_length=150, unique=True)
     slug = models.CharField(max_length=150, unique=True)
-    question = models.TextField()
+    body = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name="user_question")
+                               related_name='user_questions')
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     answerd = models.BooleanField(null=False, blank=False, default=False)
     status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
-        ''' X '''
+        """ X """
         ordering = ['-created_on']
 
     def __str__(self):
         return str(self.title) if self.title else ''
 
+
+class Comment(models.Model):
+    """ X """
+    question = models.ForeignKey(Question, on_delete=models.CASCADE,
+                             related_name='comments')
+    username = models.ForeignKey(User, on_delete=models.CASCADE,
+                                 related_name="user_comments")
+    message = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        """ X """
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f'Comment {self.message} by {self.username}'
