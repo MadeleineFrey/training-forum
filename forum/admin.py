0c164a0df0d59e3cf3dbadb7d2881f6746b2f564
Madde <Madde.frey@gmail.com>
2022-02-08 16:26:48 +0000

    Install and setup summernote

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Question
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Question)
+class QuestionAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('body')
+
