65b2b0542c17bf198a64eaa7b3cc1bc535d0dfce
Madde <Madde.frey@gmail.com>
2022-02-09 19:33:13 +0000

    Create the view code

--- forum/admin.py
+++ forum/admin.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Question)
 class QuestionAdmin(SummernoteModelAdmin):
+    """ X """
 
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('body')
 
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """ X """
+    list_display = ('username', 'message', 'question', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    # username_username???????
+    search_fields = ('username_username', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Question
 
-# Create your views here.
+
+class QuestionList(generic.ListView):
+    "" X ""
+
+    model = Question
+    queryset = Question.objects.filter(ststus=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
