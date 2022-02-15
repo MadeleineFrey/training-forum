dbe9b38319149f9cba9fff0289f046f1227cd9db
Madde <Madde.frey@gmail.com>
2022-02-15 10:49:13 +0000

    Add style to questions.html

--- forum/views.py
+++ forum/views.py
@@ -13,7 +13,7 @@ class QuestionList(generic.ListView):
     model = Question
     queryset = Question.objects.filter(status=1).order_by('-created_on')
     template_name = 'questions.html'
-    # paginate_by = 6
+    paginate_by = 6
 
 
 class HomePage(View):
